2fd372b4d37678e078cacb1867fa15498f2554d5
Dan Kolz <dan@emergentideas.com>
2023-08-01 15:37:33 -0500

    fix rename typo

--- server-js/add-data-directory.js
+++ server-js/add-data-directory.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const webhandle = require('webhandle')
+const preRunPrep = require('./page-prerun-data-loader')
 
 /**
  * Adds the directory 'data' which is a child of the project root as a source of information that
  * pages can load.
  */
 function addDataDirectory() {
-	let preRunPrep = require('./page-prerun-data-loader.js')
 	let preRun = preRunPrep({ dataFolder: path.join(webhandle.projectRoot, 'data')})
 	webhandle.pageServer.preRun.push(preRun)
 }
